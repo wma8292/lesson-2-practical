dcc8488d3f639b1675cc51b36b355391b6b4dc50
wma8292 <131907073+wma8292@users.noreply.github.com>
2023-04-26 14:14:02 -0400

    Add files via upload

---
+++ Spinning wheel.py
@@ -0,0 +1,21 @@
+from microbit import *
+
+while True:
+    display.show(Image(
+    '09990:'
+    '99099:'
+    '90909:'
+    '99099:'
+    '09990'))
+    sleep(500)
+    display.clear()
+
+    display.show(Image(
+    '09990:'
+    '90909:'
+    '99999:'
+    '90909:'
+    '09990'))
+    sleep(500)
+    display.clear()
+    sleep(1)
